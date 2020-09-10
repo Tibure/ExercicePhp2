9c279a752cc16e783d4446ef463e81756b36774c
EmBureau <emilebureau@hotmail.com>
2020-09-10 14:07:24 -0400

    Commit dernier cours ANTHO

--- template.php
+++ template.php
@@ -5,10 +5,18 @@
     <meta charset="UTF-8">
     <title>Mon site</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="main.css">
 </head>
+
 <body>
-    <?=$content?>
+<div class="container">
+<h1><?=$title?></h1>
+<?=$content?>
+<div class="row">
+<div class="col">
+</div></div>
+
     <?php require("includeJS.php");?>
+    </div>
 </body>
 </html>
