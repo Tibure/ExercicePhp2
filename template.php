061e3a8c87f86983e652078719fab34c005c0084
EmBureau <emilebureau@hotmail.com>
2020-09-10 13:37:54 -0400

    commit jordan

---
+++ template.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Mon site</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+</head>
+<body>
+    <?=$content?>;
+    <?php require("includeJS.php");?>;
+</body>
+</html>
--- userController.php
+++ userController.php
@@ -1,9 +1,12 @@
 <?php 
 
 require_once('view.php');
+require_once("userDTO.php");
+require_once("controller.php");
 
-class userController{
+class userController extends Controller {
     private $user_model;
+    private const SHOW_USERS_TITLE = "Les usagers";
     public function __construct($user_model){
         $this->user_model = $user_model;
     }
@@ -12,7 +15,8 @@ class userController{
         $users = $this->user_model->get_all_users();
         $data = array("users"=>$users);
         $view = new View("usersView.php");
-        echo $view->render($data);
+        $content = $view->render($data);
+        $this->render_template_with_content(self::SHOW_USERS_TITLE,$content);
     }
     
     public function show_one_user($id_user_get){
